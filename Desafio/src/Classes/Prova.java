2ef268f63b863031ed179b80da920bab317d64fb
Antonio Kennedy <antonioliveiraak@gmail.com>
2021-06-25 11:48:19 -0300

    Update desafio getter and setters

--- Desafio/src/Classes/Prova.java
+++ Desafio/src/Classes/Prova.java
@@ -1,10 +1,26 @@
 package Classes;
 
 public class Prova {
-    public double notaP1, notaP2;
+    private double notaP1, notaP2;
 
     public Prova(double notaP1, double notaP2){
         this.notaP1 = notaP1;
+        this.notaP2 = notaP2;
+    }
+
+    public double getNotaP1(){
+        return notaP1;
+    }
+
+    public void setNotaP1(double notaP1){
+        this.notaP1 = notaP1;
+    }
+
+    public double getNotaP2(){
+        return notaP1;
+    }
+
+    public void setNotaP2(double notaP2){
         this.notaP2 = notaP2;
     }
 
--- Desafio/src/Classes/Turma.java
+++ Desafio/src/Classes/Turma.java
@@ -1,11 +1,35 @@
 package Classes;
 
 public class Turma {
-    Aluno aluno1, aluno2, aluno3;
+    private Aluno aluno1, aluno2, aluno3;
 
     public Turma(Aluno aluno1, Aluno aluno2, Aluno aluno3){
         this.aluno1 = aluno1;
         this.aluno2 = aluno2;
+        this.aluno3 = aluno3;
+    }
+    
+    public Aluno getAluno1(){
+        return aluno1;
+    }
+
+    public void setAluno1(Aluno aluno1){
+        this.aluno1 = aluno1;
+    }
+
+    public Aluno getAluno2(){
+        return aluno2;
+    }
+
+    public void setAluno2(Aluno aluno2){
+        this.aluno2 = aluno2;
+    }
+
+    public Aluno getAluno3(){
+        return aluno3;
+    }
+
+    public void setAluno3(Aluno aluno3){
         this.aluno3 = aluno3;
     }
 
