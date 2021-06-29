bf3977ec008b91e8a7309fba935502a694d942c6
Antonio Kennedy <antonioliveiraak@gmail.com>
2021-06-29 13:40:06 -0300

    update version

--- Circunferencia/version-1/src/App.java
+++ Circunferencia/version-1/src/App.java
@@ -2,24 +2,30 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class App {	
+public class App {
+	public static final double pi = 3.14159;
+
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
-		System.out.println();
 
-		System.out.print("Enter radius: ");
+		System.out.print("\nEnter radius: ");
 		double radius = ler.nextDouble();
-		
-		double pi = 3.14159;
-		double Circumference = pi * (radius*2);
-		
-		double volume = 4.0 * pi * (radius * radius * radius)/3;
+		double Circumference = circumference(radius);
+		double volume = volume(radius);
 		
 		System.out.printf("Circumference: %.2f\n", Circumference);
 		System.out.printf("volume: %.2f\n", volume);	
 		System.out.printf("PI Value: %.2f\n", pi);
 		ler.close();
+	}
+
+	public static double circumference(double radius) {
+		return pi * (radius * 2);
+	}
+
+	public static double volume(double radius) {
+		return 4.0 * pi * (radius * radius * radius) / 3;
 	}
 
 }
